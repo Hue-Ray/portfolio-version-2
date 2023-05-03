04477bacca94f30adb1dae3807425ffc67e1c61c
Hue-Ray <kunletobi4@gmail.com>
2023-05-03 23:32:55 +0100

    changed button color

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,24 +5,30 @@ function Projects() {
   return (
     <div className='container'>
       <h1 className='heading'>Projects</h1>
-      <div>
+      <div className='projects pb-10 lg:grid-col-2'>
        <div>
        <img className='rounded-xl' src={NetflixCloneImg} alt="" />
        </div>
-        <p className='sub-heading'>Netflix Clone</p>
+      <div className='md:flex gap-8'>
+      <div className='md:w-[50%]'>
+       <p className='sub-heading'>Netflix Clone</p>
         <p className='description'>A project to polish my skill in fetching API and using the css framework ”Tailwind” as a main styling tool. This project took 2 days to complete and i am still working on it to make it better.</p>
+       </div>
+        <div>
         <p className='sup-heading'>Technologies</p>
-        <ul className='text-xs'>
+        <ul className='text-xs md:text-sm lg:text-lg'>
           <li>React Js</li>
           <li>Tailwind CSS</li>
           <li>Typescript</li>
         </ul>
+        </div>
+      </div>
        <div className='mt-4'>
       <a href='https://github.com/Hue-Ray/netflix-project'>
-      <button className='btn bg-[#50856E] p-2 px-4 mr-4 rounded-lg text-white font-light text-xs'>View Code</button>
+      <button className='btn bg-black p-2 px-4 mr-4 rounded-lg text-white font-light text-xs md:text-sm lg:text-lg'>View Code</button>
       </a>
        <a href='https://netflix-project-dusky.vercel.app/'>
-       <button className='btn bg-[#5C6D98] p-2 px-4 rounded-lg text-white font-light text-xs'>View Demo</button> 
+       <button className='btn border-2 p-2 px-4 rounded-lg text-black font-light text-xs md:text-sm lg:text-lg'>View Demo</button> 
        </a>
        </div>
       </div>
